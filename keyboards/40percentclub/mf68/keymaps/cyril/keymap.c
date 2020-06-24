54a21781ed771be8edf493b38cd7a7ede22ea22d
cyril279 <cyril.x9@gmail.com>
2020-06-24 10:22:34 -0400

    mf68/../cyril/keymap.c: Add [delete] to main key-cluster Added [Delete] to the alpha-cluster to facilitate basic-position accessibility. [Shift]+[Backspace]=[Delete]

--- keyboards/40percentclub/mf68/keymaps/cyril/keymap.c
+++ keyboards/40percentclub/mf68/keymaps/cyril/keymap.c
@@ -5,9 +5,13 @@
 #define _MEDIA 8
 #define _RESET 9
 
+enum custom_keycodes {
+    BSP_DEL = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_68_ansi(
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_INS,  KC_HOME,
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  BSP_DEL,          KC_INS,  KC_HOME,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,  KC_END,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT,          KC_UP,
@@ -35,3 +39,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,                            _______,                            _______, _______, _______,          _______, _______, _______
   )
 };
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint8_t saved_mods   = 0;
+    uint16_t       temp_keycode = keycode;
+
+    // Filter out the actual keycode from MT and LT keys.
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
+        temp_keycode &= 0xFF;
+    }
+
+    switch (temp_keycode) {
+        case BSP_DEL:
+            if (record->event.pressed) {
+                saved_mods = get_mods() & MOD_MASK_SHIFT;
+
+                if (saved_mods == MOD_MASK_SHIFT) {  // Both shifts pressed
+                    register_code(KC_DEL);
+                } else if (saved_mods) {   // One shift pressed
+                    del_mods(saved_mods);  // Remove any Shifts present
+                    register_code(KC_DEL);
+                    add_mods(saved_mods);  // Add shifts again
+                } else {
+                    register_code(KC_BSPC);
+                }
+            } else {
+                unregister_code(KC_DEL);
+                unregister_code(KC_BSPC);
+            }
+            return false;
+#ifdef RGB_MATRIX_ENABLE
+#endif
+    }
+    return true;
+}
